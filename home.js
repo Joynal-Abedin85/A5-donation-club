b3a78fcba63e7b3282432ca1483c703029fa523b
Joyanl-Abedin <kazitareq321@gmail.com>
2024-09-24 17:26:52 +0600

    complete switdh blog and history section

--- home.js
+++ home.js
@@ -11,12 +11,30 @@ function getTextById(id){
     return nums
 }
 
+// this is modal function  
+function modal(id){
+    
+    const mymodal = document.getElementById("my_modal_1")
+    mymodal.classList.remove("hidden")
+    return id
+}
 
 const donateNow = document.getElementById("donate")
 donateNow.addEventListener('click', function(){
+    
+    
     const donateAmount = getValueById("enter-amount")
     const currentDonate = getTextById("c-donate")
     const currentBalance = getTextById("c-balance")
+    if(donateAmount <= 0){
+        alert("enter positive amount")
+        return
+    }
+    if(isNaN(donateAmount)){
+        alert("enter valid number")
+        return
+    }
+    modal("my_modal_1")
     const donates = currentBalance - donateAmount
     const updateBalance = document.getElementById("c-balance");
     updateBalance.innerText = donates;
@@ -25,7 +43,8 @@ donateNow.addEventListener('click', function(){
     const newDonate = document.getElementById("c-donate");
     newDonate.innerText = updateDonate;
 
-    console.log(donates,updateDonate);
+    
+    
 })
 
 // 2nd card 
@@ -34,6 +53,14 @@ donateNow2.addEventListener('click', function(){
     const donateAmount2 = getValueById("donate-2")
     const currentDonate2 = getTextById("c-donate-2")
     const currentBalance = getTextById("c-balance")
+    if(donateAmount <= 0){
+        alert("enter positive amount")
+        return
+    }
+    if(isNaN(donateAmount)){
+        alert("enter valid number")
+        return
+    }
     const donates2 = currentBalance - donateAmount2
     const updateBalance2 = document.getElementById("c-balance");
     updateBalance2.innerText = donates2;
@@ -42,7 +69,7 @@ donateNow2.addEventListener('click', function(){
     const newDonate2 = document.getElementById("c-donate-2");
     newDonate2.innerText = updateDonate2;
 
-    console.log(donates,updateDonate);
+    
 })
 
 // 3rd card 
@@ -51,6 +78,14 @@ donateNow3.addEventListener('click', function(){
     const donateAmount3 = getValueById("donate-3")
     const currentDonate3 = getTextById("c-donate-3")
     const currentBalance = getTextById("c-balance")
+    if(donateAmount <= 0){
+        alert("enter positive amount")
+        return
+    }
+    if(isNaN(donateAmount)){
+        alert("enter valid number")
+        return
+    }
     const donates3 = currentBalance - donateAmount3
     const updateBalance3 = document.getElementById("c-balance");
     updateBalance3.innerText = donates3;
@@ -59,5 +94,55 @@ donateNow3.addEventListener('click', function(){
     const newDonate3 = document.getElementById("c-donate-3");
     newDonate3.innerText = updateDonate3;
 
-    console.log(donates,updateDonate);
+    
+})
+
+// history 
+
+const historybtn = document.getElementById("history");
+const donationbtn = document.getElementById("donation");
+historybtn.addEventListener('click',function(){
+    historybtn.classList.add('bg-lime-400')
+    historybtn.classList.remove('hover:bg-gray-300','bg-gray-100')
+    donationbtn.classList.remove('bg-lime-400');
+    donationbtn.classList.add('hover:bg-gray-300','bg-gray-100');
+
+    document.getElementById("main-sect").classList.add("hidden");
+
+    document.getElementById("history-details").classList.remove("hidden");
+})
+
+donationbtn.addEventListener('click',function(){
+    historybtn.classList.remove('bg-lime-400')
+    historybtn.classList.add('hover:bg-gray-300','bg-gray-100')
+    donationbtn.classList.add('bg-lime-400');
+    donationbtn.classList.remove('hover:bg-gray-300','bg-gray-100');
+
+    document.getElementById("main-sect").classList.remove("hidden");
+
+    document.getElementById("history-details").classList.add("hidden");
+})
+
+const blogbtn = document.getElementById("blogs-btn")
+const homebtn = document.getElementById("homes-btn")
+blogbtn.addEventListener('click',function(){
+
+    document.getElementById("main-sect").classList.add("hidden");
+
+    document.getElementById("blog-section").classList.remove("hidden");
+
+    document.getElementById("blogs-btn").classList.add("hidden");
+
+    document.getElementById("homes-btn").classList.remove("hidden");
+})
+
+homebtn.addEventListener('click',function(){
+
+    document.getElementById("main-sect").classList.remove("hidden");
+
+    document.getElementById("blog-section").classList.add("hidden");
+
+    document.getElementById("blogs-btn").classList.remove("hidden");
+
+    document.getElementById("homes-btn").classList.add("hidden");
 })
